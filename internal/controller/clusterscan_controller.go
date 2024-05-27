62b11a36339772ffab4a68fa1ffbed16349757a1
Nick <maniutin@gmail.com>
2024-05-27 05:46:53 +0200

    implement Reconciler for Job

--- internal/controller/clusterscan_controller.go
+++ internal/controller/clusterscan_controller.go
@@ -1,25 +1,12 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
 	"context"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -33,25 +20,90 @@ type ClusterScanReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=batch.my.domain,resources=clusterscans,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=batch.my.domain,resources=clusterscans/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=batch.my.domain,resources=clusterscans/finalizers,verbs=update
-
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the ClusterScan object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
 func (r *ClusterScanReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// Fetch the ClusterScan instance
+	clusterScan := &batchv1.ClusterScan{}
+	err := r.Get(ctx, req.NamespacedName, clusterScan)
+	if err != nil {
+		log.Error(err, "unable to fetch ClusterScan")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
+	// Define a new Job object
+	job := newJobForCR(clusterScan)
+
+	// Check if this Job already exists
+	found := &batchv1.ClusterScan{}
+	err = r.Get(ctx, types.NamespacedName{Name: job.Name, Namespace: job.Namespace}, found)
+	if err != nil {
+		if client.IgnoreNotFound(err) != nil {
+			log.Error(err, "unable to fetch Job")
+			return ctrl.Result{}, err
+		}
+
+		log.Info("Creating a new Job", "Job.Namespace", job.Namespace, "Job.Name", job.Name)
+		err = r.Create(ctx, job)
+		if err != nil {
+			log.Error(err, "unable to create Job")
+			return ctrl.Result{}, err
+		}
+
+		// Job created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	// Job already exists - don't requeue
+	log.Info("Skip reconcile: Job already exists", "Job.Namespace", found.Namespace, "Job.Name", found.Name)
 	return ctrl.Result{}, nil
+}
+
+// newJobForCR returns a job with the same name/namespace as the cr
+func newJobForCR(cr *batchv1.ClusterScan) *batchv1.ClusterScan {
+	labels := map[string]string{
+		"app": cr.Name,
+	}
+	return &batchv1.ClusterScan{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-job",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: batchv1.ClusterScanSpec{
+			JobTemplate: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "kube-linter",
+							Image: "stackrox/kube-linter:0.2.2",
+							Args:  []string{"lint", "../../files-to-lint"},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "dir-to-lint",
+									MountPath: "../../files-to-lint",
+								},
+							},
+						},
+					},
+					RestartPolicy: corev1.RestartPolicyOnFailure,
+					Volumes: []corev1.Volume{
+						{
+							Name: "dir-to-lint",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "../../files-to-lint",
+								},
+							},
+						},
+					},
+				},
+			},
+
+		},
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
